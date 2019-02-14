9681f6df7a8386993144aab30e2f750bb3241521
Sergio Fonseca <sefonsec@microsoft.com>
2019-02-14 18:00:30 +0000

    Add project files.

---
+++ WebappTestSQLConnectivity/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebappTestSQLConnectivity._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Test SQL Connectivity</h1>
+        <p class="lead">Just a test</p>
+    </div>
+
+    <div class="row">
+        <div>
+            Server Name:
+            <asp:TextBox ID="tServer" runat="server" Width="318px">fonsecanet.database.windows.net</asp:TextBox>
+            <br />
+            User:
+            <asp:TextBox ID="tUser" runat="server">fonsecasergio</asp:TextBox>
+            <br />
+            Password:
+            <asp:TextBox ID="tPassword" runat="server" TextMode="Password" AutoPostBack="False"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="bConnect" runat="server" Text="Connect" OnClick="bConnect_Click" />
+            <br />
+            <br />
+            <asp:Label ID="tResult" runat="server" Text=""></asp:Label>
+            <br />
+            <br />
+        </div>
+
+    </div>
+
+</asp:Content>
---
+++ WebappTestSQLConnectivity/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebappTestSQLConnectivity
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bConnect_Click(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            SqlConnectionStringBuilder.ApplicationName = "SQL Connectivity Test";
+            SqlConnectionStringBuilder.DataSource = tServer.Text;
+            SqlConnectionStringBuilder.UserID = tUser.Text;
+            SqlConnectionStringBuilder.Password = tPassword.Text;
+            SqlConnectionStringBuilder.ConnectTimeout = 5;
+
+            SqlConnection SqlConnection = new SqlConnection(SqlConnectionStringBuilder.ConnectionString);
+
+            string SQLquery = "SELECT @@SERVERNAME + ' - ' + @@VERSION AS RESULT";
+
+            SqlCommand SqlCommand = new SqlCommand(SQLquery, SqlConnection);
+            SqlCommand.CommandTimeout = 5;
+
+            try
+            {
+                SqlConnection.Open();
+                string Result = SqlCommand.ExecuteScalar().ToString();
+                tResult.ForeColor = System.Drawing.Color.Black;
+                tResult.Text = Result;
+            }
+            catch (Exception ex)
+            {
+                tResult.ForeColor = System.Drawing.Color.Red;
+                tResult.Text = ex.ToString();
+                
+            }
+            finally
+            {
+                if (SqlConnection.State != ConnectionState.Closed)
+                {
+                    SqlConnection.Close();
+                }
+            }
+
+
+        }
+    }
+}
